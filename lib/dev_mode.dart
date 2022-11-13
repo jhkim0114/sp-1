cef5da8ffe97790b5b1752293ed4784527ca9805
jiho <jhkkim0114@gmail.com>
2022-11-14 00:09:15 +0900

    update

---
+++ lib/dev_mode.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class DevMode {
+
+  /// notification_bar bool
+  /// notification_bar_color color
+  /// background_color color
+  /// bottom_animation bool
+  /// dynamic_island bool
+  /// notification sms
+  /// login_view
+  /// indicator_type
+  ///
+  ///
+  ///
+  ///
+
+  bool notificationBar = false;
+  Color notificationBarColor = Colors.blue;
+  Color backgroundColor = Colors.blue;
+  bool bottomAnimation = false;
+  bool dynimicIsland = false;
+  bool notificationSms = false;
+  bool loginView = false;
+  bool indicatorType = false;
+
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:kkb_flutter/app_scroll_behavior.dart';
+import 'package:kkb_flutter/util/app_scroll_behavior.dart';
 import 'package:kkb_flutter/route/app_pages.dart';
 import 'package:kkb_flutter/ui/theme/app_theme.dart';
 
